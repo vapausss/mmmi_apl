949509fecb2e6ee0b94e375858a177f6713fa88c
vapausss <ekaterinaswoboda@gmail.com>
2024-11-11 16:51:20 +0100

    noch mehr kommentare

--- ml5/ml5.js
+++ ml5/ml5.js
@@ -2,8 +2,8 @@ let video;
 let handPose;
 let hands = []; 
 let painting;
-let px = 0;
-let py = 0; 
+let px; //früher beide mit 0 initialisiert, sprang vom 0,0 punkt zu der erst gezeichneten linie - nervig
+let py; //so passiert es nicht mehr, dennoch schlecht dass uninitialized 
 
 function preload(){
     handPose = ml5.handPose({flipped:true});
@@ -32,6 +32,7 @@ function setup() {
 }
 
 //alle punkte werden angegeben, man kann gucken ob man weitere fingerkombinationen für was anderes nutzen können
+//für uns nicht notwendig aber gut zum nachgucken
 function drawAllPoints(){
   if(hands.length>0){
     for (let hand of hands){
@@ -53,8 +54,10 @@ function drawAllPoints(){
       }
     }
 }
-//einfache ausgabe von beiden punkten, kann man als default implementieren 
-//wenn wir weitere fingerkombinationen wählen kann man hier in einer if/case anweisung die funktionen switchen 
+//einfache ausgabe von beiden punkten, kann man als default implementieren
+//wenn wir weitere fingerkombinationen wählen kann man hier in einer if/case anweisung die funktionen switchen
+//aber eigentlich brauchen wir die funktion nicht, man hat alles was hier steht und meht in der nächsten funktion 
+//dient nur zur veranschaulichung 
 function showIndexThumb() {
   if (hands.length > 0) {
     let hand = hands[0];
@@ -77,6 +80,13 @@ function showIndexThumb() {
 }
 
 //implementation von dem malen mit beiden fingern
+
+//wichtige frage- wie kann man die eingabe löschen um was neues zu zeichen
+//nach einer erfolgreichen gestenerkennung den screen löschen?
+//eine fingerkombination fürs löschen aussuchen?
+//farben wechseln mit fingern?(in unserem fall nicht so wichtig)
+//farbe wechseln wenn eine geste erkannt wurde?
+//emoji für bestimmte gesten hinzufügen?
 function drawWithIndex(){
   if (hands.length > 0) {
     let hand = hands[0];
@@ -103,7 +113,6 @@ function drawWithIndex(){
   }
   image(painting, 0, 0);
 }
-
 
 function draw() {
   image(video, 0, 0);
